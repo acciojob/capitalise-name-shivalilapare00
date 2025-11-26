0fd081ffb2b899863c680cce52c4541f896d5c58
shivalilapare00 <90516920+shivalilapare00@users.noreply.github.com>
2025-11-26 18:44:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 function convertToUpper() {
-  let input = document.getElementById("fname");
-  input.value = input.value.toUpperCase();
+  document.getElementById("fname").value =
+    document.getElementById("fname").value.toUpperCase();
